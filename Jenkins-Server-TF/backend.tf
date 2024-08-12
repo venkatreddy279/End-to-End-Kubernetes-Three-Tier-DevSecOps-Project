b521b8cb9b9040fc775ea7953d184472db007648
venkatreddy279 <45058968+venkatreddy279@users.noreply.github.com>
2024-08-11 19:53:36 -0500

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfbucket-venkatreddy279"
-    region         = "us-east-1"
+    region         = "us-east-2"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
